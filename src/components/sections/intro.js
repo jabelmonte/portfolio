6102553838c91c459602b9203994fb3b8784af43
John Aron <johnaronbelmonte@gmail.com>
2021-01-29 22:59:12 +0800

    added a text for testing git

--- src/components/sections/intro.js
+++ src/components/sections/intro.js
@@ -3,7 +3,7 @@ import React from 'react'
 import sectionStyles from '../../styles/sectionStyles.module.css'
 
 function Intro(){
-    return <h1 className={sectionStyles.highlight}>Hi This is my Intro
+    return <h1 className={sectionStyles.highlight}>Hi This is my Intro :)
         <h1 className={sectionStyles.highlightSubtext}>and this is its description
     </h1>
     </h1>
