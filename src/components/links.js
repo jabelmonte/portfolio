29af883a7f1ad61dfca45c0624228c956c4b5524
John Aron <johnaronbelmonte@gmail.com>
2021-01-30 12:49:40 +0800

    provided mailto link

---
+++ src/components/links.js
@@ -0,0 +1,4 @@
+module.exports = {
+    email: 'johnaronbelmonte@gmail.com',
+    dummy: 'dummy-string',
+}
--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import {dummy, email} from '../links'
 
 function Contact(){
-    return <h1> Come say hi </h1>
+    return <h1>
+        <a className="email-button" href={`mailto:${email}`}>Come say hi</a>
+    </h1>
+
 }
 
 export default Contact;
