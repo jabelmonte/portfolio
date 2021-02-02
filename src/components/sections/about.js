a8ccc8da5df76bbb16adb5ecfa1e5bf97a0b8017
John Aron <johnaronbelmonte@gmail.com>
2021-02-02 23:39:43 +0800

    about content and flex

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -2,15 +2,41 @@ import React from "react"
 import Styled from "styled-components"
 
 const StyledAboutContainer = Styled.section`
+    display: flex;
+    justify-content: center;
     max-width: 900px;
-    background-color: var(--test-background-color);
+    padding: 50px;
+    font-family: 'NexaLight';
+`
+const StyledAboutTextBox = Styled.div`
+    order: 1;
+    max-width: 50%;
+`
+
+const StyledAboutTextImage = Styled.div`
+    order:2;
+    max-width: 50%;
+    width: 100%;
 `
 
 function About() {
   return (
     <StyledAboutContainer>
+      <StyledAboutTextBox>
       <h1>About Me: </h1>
-      <p className="descriptiveText">HEHEHEHEHEH</p>
+      <p className="descriptiveText">Hello! I'm John Aron Belmonte </p>
+      <br></br>
+      <p className="descriptiveText">
+        I'm currently looking for a job here in the city, whether it be on the
+        web or the funky enterprise, I'll check any opportunity that comes by.
+      </p>
+      <br></br>
+      <p className="descriptiveText">
+        I have started expanding my knowledge on the web as it really piques my
+        interests.
+      </p>
+      </StyledAboutTextBox>
+      <StyledAboutTextImage>asdasd</StyledAboutTextImage>
     </StyledAboutContainer>
   )
 }
