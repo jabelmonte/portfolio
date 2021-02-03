870a5fd508adf95e9dc58b28ce0a4197b3fb718f
John Aron <johnaronbelmonte@gmail.com>
2021-02-03 23:50:14 +0800

    added navbar

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,11 +1,65 @@
-import React from 'react'
-import scrollTo from 'gatsby-plugin-smoothscroll'
+import React from "react"
+import styled from "styled-components"
+import scrollTo from "gatsby-plugin-smoothscroll"
+
+const StyledHeader = styled.header`
+  position: fixed;
+  top: 0;
+  padding: 0, 50px;
+  width: 100%;
+  height: 4em;
+  background-color: pink;
+`
+const StyledNav = styled.nav`
+    display: flex;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background-color: green;
+`
+const StyledLogo = styled.div`
+    height: 100%;
+    flex-grow: 6;
+  margin-left: auto;
+  justify-content: flex-start;
+  background-color: blue;
+`
+const StyledLinks = styled.div`
+    height: 100%;
+    flex-grow: 4;
+  margin-right: auto;
+  justify-content: flex-end;
+  align-items: center;
+  background-color: violet;
+  ul{
+      list-style-type: none;
+      margin: 0;
+      padding: 50;
+      background-color: yellow;
+
+      li{
+          margin: 0;
+          position: relative;
+          font-size: var(--fs-xl);
+      }
+  }
+`
 
 function Nav() {
-    return <nav>
-        <button onClick={()=> scrollTo('#contact')}>Contact</button>
-        Hello?
+  return (
+    <nav>
+      <StyledHeader>
+        <StyledNav>
+          <StyledLogo>Hello?</StyledLogo>
+          <StyledLinks>
+            <li><button onClick={() => scrollTo("#about")}>About</button></li>
+            <li><button onClick={() => scrollTo("#contact")}>Job</button></li>
+            <li><button onClick={() => scrollTo("#contact")}>Contact</button></li>
+          </StyledLinks>
+        </StyledNav>
+      </StyledHeader>
     </nav>
+  )
 }
 
 export default Nav
--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -19,11 +19,11 @@ const StyledAboutTextImage = Styled.div`
     width: 100%;
 `
 
-function About() {
+function About({about}) {
   return (
     <StyledAboutContainer>
       <StyledAboutTextBox>
-      <h1>About Me: </h1>
+      <h1 id="about">About Me: </h1>
       <p className="descriptiveText">Hello! I'm John Aron Belmonte </p>
       <br></br>
       <p className="descriptiveText">
