47cf5532fc1610f17dd4fa3a3f8aadd91bfc0a2e
John Aron <johnaronbelmonte@gmail.com>
2021-02-17 15:52:50 +0800

    intro restructure

--- src/components/sections/intro.js
+++ src/components/sections/intro.js
@@ -3,26 +3,26 @@ import styled from "styled-components"
 
 function Intro() {
   return (
-    <StyledIntroContainer>
-      <StyledIntroHeadings>
+    <IntroContainer>
+      <IntroHeadings>
         <p>Hi i'm</p>
-        <h2>JOHN ARON</h2> 
-      </StyledIntroHeadings>
-      <StyledIntroH2>
+        <h2>JOHN ARON BELMONTE</h2>
         <h3>I build things of interest</h3>
-      </StyledIntroH2>
-      <p>
+      </IntroHeadings>
+      <IntroTexts>
+        <p>
         I'm a software engineer living in the
         <span className="importantText"> city of Quezon</span>,
         <br></br>
         I continue learning technologies that could benefit with the internet
       </p>
-    </StyledIntroContainer>
+      </IntroTexts>
+    </IntroContainer>
   )
 }
 
-const StyledIntroContainer = styled.section`
-  max-width: 900px;
+const IntroContainer = styled.section`
+  max-width: 100%;
   height: 80%vh;
   margin: 0 auto;
   border-width: 0;
@@ -30,6 +30,10 @@ const StyledIntroContainer = styled.section`
   margin-bottom: 16em;
   width: 100%;
   padding: 5px;
+  display: block;
+  justify-content: center;
+  text-align: center;
+
   p {
       font-size: 1.2rem;
       font-family: 'NexaBold';
@@ -40,6 +44,10 @@ const StyledIntroContainer = styled.section`
      justify-content: center;
      text-align: center;
      padding: 2px;
+
+     h2 {
+       font-size: 3em;
+     }
     h3 {
       font-size: 3rem;
     }
@@ -50,17 +58,19 @@ const StyledIntroContainer = styled.section`
     flex-direction: column;
     justify-content: center;
     text-align: center;
-
+    h2 {
+       font-size: 2.5em;
+     }
     h3 {
       font-size: 2rem;
     }
    }
 `
-const StyledIntroHeadings = styled.div`
+const IntroHeadings = styled.div`
   margin: 0 auto;
   padding: 0;
 `
-const StyledIntroH2 = styled.div`
+const IntroTexts = styled.div`
   margin: 0 auto;
   padding: 0;
   font-size: 1px;
